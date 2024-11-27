06aa4789769db0a074de525ce74e5957910651bc
hqr <hqr114514@outlook.com>
2024-11-27 12:40:42 +0800

    base

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "sha512.h"
+
+int main(int argc, char** argv) {
+	if (argc < 2) return 1;
+	char* FileName = argv[1];
+	uc out[64] = { 0 };
+	sha512(FileName, out);
+	for (int i = 0; i < 32; i++) printf("%02x", out[i]);
+	cout << endl;
+	return 0;
+}
---
+++ sha512.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ui unsigned int
+#define ull unsigned long long
+#define uc unsigned char
+
+void block(uc data[64]);
+void sha512(const char* FileName, uc* out);
